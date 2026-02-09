aa7817d7498bced3353cceab07148bc13964676c
Firmansyah <firmansyahwork2@gmail.com>
2026-02-09 13:26:09 +0700

    Belajar Python pertama: Program Perkenalan

---
+++ main.py
@@ -0,0 +1,6 @@
+nama = "Firmansyah"
+pekerjaan = "Admin Klinik"
+target = "Back-End Developer"
+
+print(f"Halo, nama saya {nama}. Saat ini saya bekerja sebagai {pekerjaan}.")
+print(f"Saya sedang belajar Python untuk menjadi {target}!")
